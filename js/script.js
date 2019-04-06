84020c9112d52949b3147d2a931d971e0faf54ad
sarisri <47717190+sarisri@users.noreply.github.com>
2019-04-06 22:23:27 +0100

    script.js
    Removed javascript for sticky nav

--- js/script.js
+++ js/script.js
@@ -1,13 +1,3 @@
 $(document).ready(function() {
-    var yourNavigation = $(".nav");
-    stickyDiv = "sticky";
-    yourHeader = $('.header').height();
 
-$(window).scroll(function() {
-  if( $(this).scrollTop() > yourHeader ) {
-    yourNavigation.addClass(stickyDiv);
-  } else {
-    yourNavigation.removeClass(stickyDiv);
-  }
-});
 }
