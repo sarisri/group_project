71d7f0526a61bf467b8364b297ceed213950ea37
sarisri <47717190+sarisri@users.noreply.github.com>
2019-04-06 18:21:22 +0100

    Header & nav
    Inserted image in header
    Made nav center
    Added header & nav & footer to all html pages
    Added script.js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    var yourNavigation = $(".nav");
+    stickyDiv = "sticky";
+    yourHeader = $('.header').height();
+
+$(window).scroll(function() {
+  if( $(this).scrollTop() > yourHeader ) {
+    yourNavigation.addClass(stickyDiv);
+  } else {
+    yourNavigation.removeClass(stickyDiv);
+  }
+});
+}
